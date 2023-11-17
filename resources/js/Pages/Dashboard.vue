781b224112f2aee1432a7a155a22080f64ff3302
Razvan-BT <spatarurazvan8@gmail.com>
2023-11-17 09:59:45 +0200

    -

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -42,7 +42,7 @@ export default {
     },
     methods: {
         async sendData() {
-            if (this.configurator_name != '') {
+            if(this.configurator_name != '') {
                 let data = {
                     templateName: this.configurator_name,
                     templateDetails: this.configurator_details,
@@ -69,7 +69,7 @@ export default {
             let dataResponse;
             await axios.post(url, data).then(function (response) {
                 dataResponse = response;
-            }).catch(function (error) {
+            }). catch(function(error) {
                 console.log("[Error from API]: ", error);
             });
             return dataResponse;
@@ -80,95 +80,121 @@ export default {
 
 
 <template>
-    <Head title="Main" />
+    <Head title="Main"/>
     <AuthenticatedLayout>
-        <Toast />
+            <Toast />
 
         <div class="card px-8">
+            <!-- <DataTable :value="configurators">
+                <Column header="Image">
+                    <template #body="slotProps">
+                        <img :src="`${slotProps.data.image}`" :alt="slotProps.data.image" class="w-6rem shadow-2 border-round" />
+                    </template>
+                </Column>
+                <template #header>
+                    <div class="flex flex-wrap align-items-center justify-content-between gap-2">
+                        <span class="text-xl text-900 font-bold">Configurators</span>
+                        <PrimarButton @click.stop="visible = true">
+                            <i class="pi pi-plus" style="font-size: 1rem"></i>
+                        </PrimarButton>
+                    </div>
+                </template>
+                <Column field="configurator_title" header="Name"></Column>
+                <Column field="configurator_detail" header="Details"></Column>
+                <Column field="configurator_id" header="ID"></Column>
+                <Column header="Actions">
+                    <template #body="slotProps">
+                        <div class="card flex gap-3">
+                            <div class="card flex flex-wrap justify-content-between">
+                                <Button label="Edit" icon="pi pi-spin pi-cog"
+                                @click="route('testt')"
+                                />
+                                <Button label="Delete" icon="pi pi-trash" 
+                            
+                                />
+                            </div>
 
-            <div class="flex flex-wrap align-items-center justify-content-between gap-2 mt-3">
-                <span class="text-xl text-900 font-bold">Configurators</span>
-                <PrimarButton @click.stop="visible = true">
-                    <i class="pi pi-plus" style="font-size: 1rem"></i>
-                </PrimarButton>
-            </div>
-            <!-- tabel optiuni -->
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">Image</th>
-                        <th scope="col">Configurators</th>
-                        <th scope="col">Details</th>
-                        <th scope="col">ID</th>
-                        <th scope="col">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="(items) in configurators">
-                        <th>
-                            <div class="p-2">
+                        </div>
+                    </template>
+                </Column>
+                <template #footer> In total there are {{ configurators ? configurators.length : 0 }} products. </template>
+            </DataTable> -->
+
+             <!-- tabel optiuni -->
+             <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Image</th>
+                            <th scope="col">Configurators</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Details</th>
+                            <th scope="col">ID</th>
+                            <th scope="col">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(items) in configurators">
+                            <th scope="row">
                                 <img :src="`${items.image}`" :alt="items.image" class="w-6rem shadow-2 border-round" />
+                            </th>
+                            <td>{{ items.configurator_title }}</td>
+                            <td>{{ items.configurator_name }}</td>
+                            <td>{{ items.configurator_detail }}</td>
+                            <td>{{ items.configurator_id }}</td>
+                            <td>
+                                <div class="card flex gap-3">
+                                <div class="card flex flex-wrap justify-content-between">
+                                <Button label="Edit" icon="pi pi-spin pi-cog"
+                                @click="route('testt')"
+                                />
+                                <Button label="Delete" icon="pi pi-trash" 
+                            
+                                />
                             </div>
-                        </th>
-                        <td>
-                            <div class="p-2">
-                                {{ items.configurator_title }}
-                            </div>
-                        </td>
-                        <td>
-                            <div class="p-2">
 
-                                {{ items.configurator_detail }}
-                            </div>
-                        </td>
-                        <td>
-                            <div class="p-2">
+                        </div>
 
-                                {{ items.configurator_id }}
-                            </div>
-                        </td>
-                        <td>
-                            <div class="d-flex flex-row-reverse">
-                                <div class="p-2">
-                                    <Button label="Edit" icon="pi pi-spin pi-cog" @click="route('testt')" />
-                                </div>
-                                <div class="p-2">
-                                    <Button label="Delete" icon="pi pi-trash" />
-                                </div>
-                                <div class="p-2">
-                                    <Button label="Delete" icon="pi pi-trash" />
-                                </div>
-                            </div>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
 
-            <Dialog v-model:visible="visible" modal header="Create new" :style="{ width: '40vw' }">
-                <form @submit.prevent="submit">
-                    <div class="p-3">
-                        <InputLabel for="configurator_name" value="Configurator Title" />
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
 
-                        <TextInput v-model="configurator_name" type="text" class="mt-1 block w-full" autofocus
-                            autocomplete="configurator_name" />
+        <Dialog v-model:visible="visible" modal header="Create new" :style="{ width: '40vw' }">
+            <form @submit.prevent="submit">
+                <div class="p-3">
+                    <InputLabel for="configurator_name" value="Configurator Title" />
 
-                    </div>
+                    <TextInput
+                        v-model="configurator_name"
+                        type="text"
+                        class="mt-1 block w-full"
+                        autofocus
+                        autocomplete="configurator_name"
+                    />
 
-                    <div class="p-3">
-                        <InputLabel for="configurator_name" value="Configurator Details" />
+                </div>
 
-                        <TextInput v-model="configurator_details" type="text" class="mt-1 block w-full" autofocus
-                            autocomplete="configurator_details" />
+                <div class="p-3">
+                    <InputLabel for="configurator_name" value="Configurator Details" />
 
-                    </div>
-                    <div class="flex items-center justify-end mt-4">
-                        <PrimaryButton @click="sendData" class="ml-4">
-                            add
-                        </PrimaryButton>
-                    </div>
-                </form>
+                    <TextInput
+                        v-model="configurator_details"
+                        type="text"
+                        class="mt-1 block w-full"
+                        autofocus
+                        autocomplete="configurator_details"
+                    />
 
-            </Dialog>
-        </div>
+                </div>
+                <div class="flex items-center justify-end mt-4">
+                    <PrimaryButton @click="sendData" class="ml-4">
+                        add
+                    </PrimaryButton>
+                </div>
+            </form>
+
+        </Dialog>
+    </div>
     </AuthenticatedLayout>
 </template>
