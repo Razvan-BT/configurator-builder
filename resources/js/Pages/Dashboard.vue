238e2bd317b79d85a404f6050dc75bb284e6fe0d
Razvan-BT <spatarurazvan8@gmail.com>
2024-03-20 13:44:34 +0200

    design

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -44,7 +44,7 @@ export default {
     },
     methods: {
         async sendData() {
-       
+
             if (this.configurator_name != '') {
                 let data = {
                     templateName: this.configurator_name,
@@ -59,8 +59,8 @@ export default {
                 this.configurator_shopify = '';
                 this.visible = false;
 
-                    // console.log(response.data.data.configurator_id)
-                if(response.status === 200) {
+                // console.log(response.data.data.configurator_id)
+                if (response.status === 200) {
                     let details = 'Configurator ' + response.data.data.configurator_id + ' was added';
                     this.$toast.add({ severity: 'info', summary: 'Info Message', detail: details, life: 3000 });
 
@@ -69,7 +69,7 @@ export default {
                     }, 4000);
                 }
             }
-            
+
         },
 
         editConfigurator(n, i) {
@@ -89,8 +89,8 @@ export default {
                 by: usePage().props.auth.user.name
             });
 
-            if(response.status === 200) {
-                switch(response.data.status) {
+            if (response.status === 200) {
+                switch (response.data.status) {
                     case 0: {
                         this.$toast.add({ severity: 'error', summary: 'Info Message', detail: response.data.message, life: 3000 });
                         break;
@@ -114,12 +114,12 @@ export default {
                 by: usePage().props.auth.user.name
             });
 
-            if(response.status == 200) {
+            if (response.status == 200) {
                 this.$toast.add({ severity: 'error', summary: 'Info Message', detail: response.data.status, life: 3000 });
             }
 
             this.configurators.filter((value, index) => {
-                if(value.configurator_id == id) {
+                if (value.configurator_id == id) {
                     this.configurators.splice(index, 1);
                 }
             });
@@ -140,102 +140,112 @@ export default {
 
 
 <template>
+
     <Head title="Main" />
     <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Configurators</h2>
+        </template>
         <Toast />
 
-        <div class="card px-8">
 
-            <!-- tabel optiuni -->
-            <div class="flex flex-wrap align-items-center justify-content-between gap-2 mt-3">
-                <span class="text-xl text-900 font-bold">Configurators</span>
-                <SuccesButton @click.stop="visible = true">
-                    <span class="px-1">Create new</span>
-                    <i class="pi pi-plus" style="font-size: 1rem"></i>
-                </SuccesButton>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+                <!-- tabel optiuni -->
+                <div class="flex flex-wrap align-items-center justify-content-end gap-2 mt-3">
+                    <SuccesButton @click.stop="visible = true">
+                        <span class="px-1">Create new</span>
+                        <i class="pi pi-plus" style="font-size: 1rem"></i>
+                    </SuccesButton>
+                </div>
+                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                    <section class="">
+
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <!-- <th scope="col">Image</th> -->
+                                    <th scope="col">Title</th>
+                                    <!-- <th scope="col">Description</th> -->
+                                    <th scope="col">ID</th>
+                                    <th scope="col">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="(items) in configurators">
+                                    <!-- <th scope="row">
+                                            <img :src="`${items.image}`" :alt="items.image" class="w-6rem shadow-2 border-round" />
+                                        </th> -->
+                                    <td>{{ items.configurator_title }}</td>
+                                    <!-- <td>{{ items.configurator_detail }}</td> -->
+                                    <td data-toggle="tooltip" data-placement="bottom"
+                                        :title="`${items.configurator_title}`">{{
+                        items.configurator_id }}</td>
+                                    <td>
+                                        <div class="d-flex flex-center">
+                                            <div class="p-2"><Button label="Edit" icon="pi pi-spin pi-cog"
+                                                    @click="routeToConf(items.configurator_id)" />
+                                            </div>
+                                            <div class="p-2"><Button label="Clone" icon="pi pi-clone"
+                                                    @click="duplicateConfigurator(items.configurator_id, items.configurator_title, items.configurator_detail)" />
+                                            </div>
+                                            <div class="p-2"><Button label="Delete" icon="pi pi-trash"
+                                                    @click="deleteConf(items.configurator_id)" /></div>
+                                            <!-- <div class="p-2"><Button label="Clone" icon="pi pi-clone" /></div> -->
+                                        </div>
+
+
+
+
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </section>
+                </div>
             </div>
-            <div class="d-flex justify-content-center">
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <!-- <th scope="col">Image</th> -->
-                            <th scope="col">Title</th>
-                            <!-- <th scope="col">Description</th> -->
-                            <th scope="col">ID</th>
-                            <th scope="col">Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="(items) in configurators">
-                            <!-- <th scope="row">
-                                <img :src="`${items.image}`" :alt="items.image" class="w-6rem shadow-2 border-round" />
-                            </th> -->
-                            <td>{{ items.configurator_title }}</td>
-                            <!-- <td>{{ items.configurator_detail }}</td> -->
-                            <td data-toggle="tooltip"  data-placement="bottom" :title="`${items.configurator_title}`" >{{ items.configurator_id }}</td>
-                            <td>
-                                <div class="d-flex flex-center">
-                                    <div class="p-2"><Button label="Edit" icon="pi pi-spin pi-cog"
-                                            @click="routeToConf(items.configurator_id)" />
-                                    </div>
-                                    <div class="p-2"><Button label="Clone" icon="pi pi-clone"
-                                            @click="duplicateConfigurator(items.configurator_id, items.configurator_title, items.configurator_detail)" />
-                                    </div>
-                                    <div class="p-2"><Button label="Delete" icon="pi pi-trash" @click="deleteConf(items.configurator_id)"/></div>
-                                    <!-- <div class="p-2"><Button label="Clone" icon="pi pi-clone" /></div> -->
-                                </div>
-
-
-
-
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-
-            <Dialog v-model:visible="visible" modal header="Create new" :style="{ width: '40vw' }">
-                <form @submit.prevent="submit">
-                    <div class="p-3">
-                        <InputLabel for="configurator_name" value="Configurator Title" />
-
-                        <TextInput v-model="configurator_name" type="text" class="mt-1 block w-full" autofocus
-                            autocomplete="configurator_name" />
-
-                    </div>
-
-                    <div class="p-3">
-                        <InputLabel for="configurator_name" value="Configurator Details" />
-
-                        <TextInput v-model="configurator_details" type="text" class="mt-1 block w-full" autofocus
-                            autocomplete="configurator_details" />
-
-                    </div>
-
-                    <div class="p-3">
-
-                        <h2 class="text-lg font-medium text-gray-900">Shopify</h2>
-
-                        <p class="mt-1 mb-1 text-sm text-gray-600">
-                            Import data from shopify using only a valid configurator ID.
-                        </p>
-
-                        <InputLabel for="configurator_shopify" value="Shopify configurator ID" />
-
-                        <TextInput v-model="configurator_shopify" type="text" class="mt-1 block w-full" autofocus
-                            autocomplete="configurator_shopify" />
-
-                    </div>
-
-                    <div class="flex items-center justify-end mt-4">
-                        <PrimaryButton @click="sendData" class="ml-4">
-                            add
-                        </PrimaryButton>
-                    </div>
-                </form>
-
-            </Dialog>
         </div>
+        <Dialog v-model:visible="visible" modal header="Create new" :style="{ width: '40vw' }">
+            <form @submit.prevent="submit">
+                <div class="p-3">
+                    <InputLabel for="configurator_name" value="Configurator Title" />
+
+                    <TextInput v-model="configurator_name" type="text" class="mt-1 block w-full" autofocus
+                        autocomplete="configurator_name" />
+
+                </div>
+
+                <div class="p-3">
+                    <InputLabel for="configurator_name" value="Configurator Details" />
+
+                    <TextInput v-model="configurator_details" type="text" class="mt-1 block w-full" autofocus
+                        autocomplete="configurator_details" />
+
+                </div>
+
+                <div class="p-3">
+
+                    <h2 class="text-lg font-medium text-gray-900">Shopify</h2>
+
+                    <p class="mt-1 mb-1 text-sm text-gray-600">
+                        Import data from shopify using only a valid configurator ID.
+                    </p>
+
+                    <InputLabel for="configurator_shopify" value="Shopify configurator ID" />
+
+                    <TextInput v-model="configurator_shopify" type="text" class="mt-1 block w-full" autofocus
+                        autocomplete="configurator_shopify" />
+
+                </div>
+
+                <div class="flex items-center justify-end mt-4">
+                    <PrimaryButton @click="sendData" class="ml-4">
+                        add
+                    </PrimaryButton>
+                </div>
+            </form>
+
+        </Dialog>
 
     </AuthenticatedLayout>
 </template>
